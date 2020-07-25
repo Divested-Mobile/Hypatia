c577c0f20579e859b75c8ff674b4c8f0689eb289
Tad <tad@spotco.us>
2020-07-25 09:21:38 -0400

    Add reference scripts for generating the databases

---
+++ scripts/clamav.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#sudo freshclam
+mkdir /tmp/mss
+cd /tmp/mss
+cp /var/lib/clamav/main.cvd .
+cp /var/lib/clamav/daily.cld .
+sigtool -u main.cvd
+sigtool -u daily.cld
+
+#MD5
+grep "Andr\\." main.hdb >> Android.hdb
+grep "Andr\\." daily.hdb >> Android.hdb
+grep "Unix\\." main.hdb >> Android.hdb
+grep "Unix\\." daily.hdb >> Android.hdb
+grep "Multios\\." main.hdb >> Android.hdb
+grep "Multios\\." daily.hdb >> Android.hdb
+
+#SHA
+grep "Andr\\." main.hsb >> Android.hsb
+grep "Andr\\." daily.hsb >> Android.hsb
+grep "Unix\\." main.hsb >> Android.hsb
+grep "Unix\\." daily.hsb >> Android.hsb
+grep "Multios\\." main.hsb >> Android.hsb
+grep "Multios\\." daily.hsb >> Android.hsb
+
+gzip /tmp/mss/*.hdb
+gzip /tmp/mss/*.hsb
